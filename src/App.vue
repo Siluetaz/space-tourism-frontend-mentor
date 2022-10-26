d1cde5cb0f43c61c7653f2f6b3cf9017ea3e0314
Siluetaz <ysmaelborquezz@gmail.com>
2022-10-26 03:53:14 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<script setup>
+import Home from "./components/Home.vue";
+</script>
+
+<template>
+  <div class="container">
+    <nav class="navbar">
+      <div class="img-logo">
+        <img src="./../assets/shared/logo.svg" alt="">
+      </div>
+      <hr class="line">
+      <div class="nav">
+        <div class="nav-options">
+          <div class="nav-btn"><a href="">
+              <strong> 00</strong> HOME
+            </a>
+          </div>
+          <div class="nav-btn">
+            <a href=""><strong>01</strong> DESTINATION</a>
+          </div>
+          <div class="nav-btn">
+            <a href=""><strong>02</strong> CREW</a>
+          </div>
+          <div class="nav-btn">
+            <a href=""><strong>03</strong> TECHNOLOGY</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <home></home>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  height: 100vh;
+
+  .navbar {
+    position: fixed;
+    right: 0rem;
+    left: 0;
+    top: 3.6rem;
+    display: grid;
+    grid-template-columns: repeat(19, minmax(0, 1fr));
+    align-items: center;
+
+    .img-logo {
+      grid-column: 1/3;
+      grid-row: 1/1;
+      text-align: center;
+    }
+
+    .line {
+      grid-column: 3/9;
+      grid-row: 1/1;
+      z-index: 1;
+      transform: translateX(5%);
+      opacity: 20%;
+      width: 100%;
+    }
+
+    //min-width: 54rem;
+    .nav {
+      grid-column: 9/20;
+      grid-row: 1/1;
+      background: rgba(255, 255, 255, 0.04);
+      backdrop-filter: blur(40.7742px);
+      justify-content: center;
+      display: grid;
+      grid-template-columns: repeat(23, minmax(0, 1fr));
+      align-items: center;
+
+      .nav-options {
+        grid-column: 4/24;
+        display: flex;
+        gap: 5%;
+
+        .nav-btn {
+          letter-spacing: 0.25rem;
+          height: 9rem;
+          align-items: center;
+          display: flex;
+          cursor: pointer;
+          transition: .3s;
+
+          &:hover {
+            box-shadow: inset 0px -0.3rem rgb(204, 204, 204);
+          }
+          &:active {
+            box-shadow: inset 0px -0.3rem white;
+          }
+
+          a {
+            text-decoration: none;
+            color: white;
+          }
+        }
+
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,91 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="background"></div>
+    <div class="content">
+        <div class="text-side">
+            <div class="text">
+                <div class="slogan">SO, YOU WANT TO TRAVEL TO</div>
+                <div class="title">SPACE</div>
+                <div class="description">Let's face it; if you want to go to space, you might as well genuinely go to
+                    outer
+                    space and not hover kind of on the
+                    edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!
+                </div>
+            </div>
+        </div>
+        <div class="explore-side">
+            <button class="btn-explore">EXPLORE</button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.content {
+    display: flex;
+    color: white;
+    height: 80%;
+    align-items: flex-end;
+    justify-content: space-around;
+
+    .text-side {
+        .text {
+            display: flex;
+            flex-direction: column;
+            gap: 1rem;
+
+            .slogan {
+                font-size: 28px;
+                letter-spacing: 4.72px;
+            }
+
+            .title {
+                font-family: 'Bellefair';
+                font-size: 150px;
+                margin-bottom: 1rem;
+            }
+
+            .description {
+                font-family: 'Barlow';
+                font-size: 18px;
+                letter-spacing: 0.1rem;
+                width: 45rem;
+            }
+        }
+    }
+
+    .explore-side {
+        .btn-explore {
+            width: 22rem;
+            height: 22rem;
+            border-radius: 900px;
+            font-family: 'Bellefair';
+            font-size: 2rem;
+            letter-spacing: 0.2rem;
+            border: none;
+            background-color: white;
+
+
+            cursor: pointer;
+        }
+
+        &:hover {
+            border-radius: 900px;
+            animation: hover-btn-explore 0.5s forwards;
+
+        }
+    }
+}
+
+.background {
+    width: 100%;
+    height: 100%;
+    background-image: url(./../../assets/home/background-home-desktop.jpg);
+    background-size: cover;
+    position: absolute;
+    z-index: -1;
+}
+
+</style>
